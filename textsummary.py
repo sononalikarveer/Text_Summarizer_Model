d1781e563015e3080514a88151078fc18df7ae37
sosonali <sonalikarveerkarveer@gmail.com>
2026-05-27 10:22:49 +0530

    This is Text Summarizer first commit having the requirements and model downloaded in it

---
+++ textsummary.py
@@ -0,0 +1,31 @@
+import torch 
+import gradio as gr
+from transformers import pipeline
+
+pipe = pipeline(
+    "text-generation", 
+    model="ClarityClips/ClarityQwen2Summarizer", 
+    dtype=torch.bfloat16,                       
+    device_map="auto")
+
+# 2. Define your text
+text = "The Defence Research and Development Organisation (DRDO) is an agency under the Department of Defence Research and Development in the Ministry of Defence of the Government of India, charged with the military's research and development, headquartered in New Delhi, India. "
+
+prompt = f"<|im_start|>user\nSummarize the following text:\n{text}<|im_end|>\n<|im_start|>assistant\n"
+
+pipe.model.generation_config.max_length = None
+
+outputs = pipe(
+    prompt, 
+    max_new_tokens=150, 
+    do_sample=False
+)
+
+generated_text = outputs[0]['generated_text']
+
+summary = generated_text.split("<|im_start|>assistant\n")[-1]
+
+summary = summary.split("<|im_end|>")[0].strip()
+
+print("\n--- Model Summary ---")
+print(summary)
