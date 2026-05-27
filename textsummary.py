96ceadc52fe3b326438a41d49e4f391e66c7b0ed
sosonali <sonalikarveerkarveer@gmail.com>
2026-05-27 15:52:58 +0530

    Gradio  Interface is added

--- textsummary.py
+++ textsummary.py
@@ -2,35 +2,49 @@ import torch
 import gradio as gr
 from transformers import pipeline
 
-
-model_id="ClarityClips/ClarityQwen2Summarizer"
-
 pipe = pipeline(
     "text-generation", 
-    model=model_id, 
+    model="ClarityClips/ClarityQwen2Summarizer", 
     dtype=torch.bfloat16,                       
     device_map="auto",
     clean_up_tokenization_spaces=False
 )
 
 # 2. Define your text
-text = "The Defence Research and Development Organisation (DRDO) is an agency under the Department of Defence Research and Development in the Ministry of Defence of the Government of India, charged with the military's research and development, headquartered in New Delhi, India. "
+#text = "The Defence Research and Development Organisation (DRDO) is an agency under the Department of Defence Research and Development in the Ministry of Defence of the Government of India, charged with the military's research and development, headquartered in New Delhi, India. "
 
-prompt = f"<|im_start|>user\nSummarize the following text:\n{text}<|im_end|>\n<|im_start|>assistant\n"
+#prompt = f"<|im_start|>user\nSummarize the following text:\n{text}<|im_end|>\n<|im_start|>assistant\n"
 
 pipe.model.generation_config.max_length = None
 
-outputs = pipe(
-    prompt, 
-    max_new_tokens=150, 
-    do_sample=False
-)
+#outputs = pipe(
+#     prompt, 
+#     max_new_tokens=150, 
+#     do_sample=False
+# )
 
-generated_text = outputs[0]['generated_text']
+#generated_text = outputs[0]['generated_text']
 
-summary = generated_text.split("<|im_start|>assistant\n")[-1]
+#summary = generated_text.split("<|im_start|>assistant\n")[-1]
 
-summary = summary.split("<|im_end|>")[0].strip()
+#summary = summary.split("<|im_end|>")[0].strip()
 
-print("\n--- Model Summary ---")
-print(summary)
+#print("\n--- Model Summary ---")
+#print(text_summary(summary))
+
+
+def summarize_text(input):
+    output = pipe(input,max_new_tokens=150)
+    return output[0]['generated_text']
+
+gr.close_all()
+
+#demo = gr.Interface(fn=summarize_text, inputs="text", outputs="text")
+
+demo = gr.Interface(fn=summarize_text, inputs=[gr.Textbox(label="Enter Input to summarize", lines=6)],
+outputs=[gr.Textbox(label="Summarized Text", lines=4)], title="@TextSummarizer", description="IF WANT TO SUMMARIZE TEXT THEN U R AT RIGHT PLACE!!")
+
+demo.launch()
+
+
+
